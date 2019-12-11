93867d6f28001d0f396633b83f6ce92dbb1360a7
David Wright <720482+dkwgit@users.noreply.github.com>
2019-12-11 14:15:45 -0600

    Day 8-2

--- Day8/Driver.py
+++ Day8/Driver.py
@@ -1,33 +1,51 @@
 from DataFixture import DataFixture as DataFixture
 from Image import Image as Image
 
-class Driver:
+#https://adventofcode.com/2019/day/8/
 
+class Driver:
     def GetImage(self):
-        columns, rows, rawData = DataFixture.Day7Part1MainData
-        image = Image(columns, rows, rawData)
+        rows, columns, rawData = DataFixture.Day7Part1MainData
+        image = Image(rows, columns, rawData)
         return image
     
-    def GetLayerDigitCounts(self, image):
-        counts = []
+    def GetDigitCountsByLayer(self, image):
+        countable = self.GetDigitCountInfo(image)
+        digitCountsByLayer = self.CountDigits(countable)
+        return digitCountsByLayer
+
+    def GetDigitCountInfo(self, image):
+        #mark 1 for each digit to make it easy to count
+        countable = []
         for index,layer in enumerate(image.GetLayers()):
-            counts.append(list(map(lambda x: (
+            countable.append(list(map(lambda x: (
                 1 if int(x) == 0 else 0,
                 1 if int(x) == 1 else 0,
                 1 if int(x) == 2 else 0,
                 int(x)), layer)))
-        return counts
+        return countable
+    
+    def CountDigits(self, digitCounts):
+        digitSums = []
+        for layerIndex, layer in enumerate(digitCounts):
+            zeroes = sum(map(lambda x: x[0], layer))
+            ones = sum(map(lambda y: y[1], layer))
+            twos = sum(map(lambda z: z[2], layer))
+            digitSums.append((zeroes, ones, twos, layerIndex))
+        return digitSums
 
 d = Driver()
 image = d.GetImage()
 image.Process()
-digitCounts = d.GetLayerDigitCounts(image)
-digitSums = []
-for index, layer in enumerate(digitCounts):
-    zeroes = sum(map(lambda x: x[0], layer))
-    ones = sum(map(lambda y: y[1], layer))
-    twos = sum(map(lambda z: z[2], layer))
-    digitSums.append((zeroes,ones,twos, index))
-digitSums.sort()
-lowestZeroes = digitSums[0]
-print(f"Layer with lowest number of 0s is layer {lowestZeroes[3]}, with {lowestZeroes[0]} 0s. 1s and 2s multipled are {lowestZeroes[1] * lowestZeroes[2]}")
+
+#Day8-1
+digitsCountsByLayer = d.GetDigitCountsByLayer(image)
+#digits are now in tuples (zeroesCount, onesCount, twosCount, layerIndex)
+#sorting sort by count of zeroes since it's first in tuple, so smallest number of zeroes will be at index 0
+digitsCountsByLayer.sort()  
+lowestZeroesLayer = digitsCountsByLayer[0]
+print(f"Day 8-1: Layer with lowest number of 0s is layer {lowestZeroesLayer[3]}, with {lowestZeroesLayer[0]} 0s. 1s and 2s multipled are {lowestZeroesLayer[1] * lowestZeroesLayer[2]}")
+
+#Day8-1
+print("\nDay 8-2: Password is")
+image.Render()
--- Day8/Image.py
+++ Day8/Image.py
@@ -1,17 +1,53 @@
 class Image:
 
-    def __init__(self,columns, rows, rawData):
-        self._columns = columns
+    def __init__(self,rows, columns, rawData):
         self._rows = rows
+        self._columns = columns
         self._rawData = rawData
         self._layers = []
+        self._renderLayerPixels = []
 
     def Process(self):
-        assert(len(self._rawData)  % (self._columns * self._rows) == 0)
-        numberOfLayers = int(len(self._rawData) / (self._columns * self._rows))
-        for i in range(0,numberOfLayers):
-            self._layers.append(self._rawData[i*(self._columns * self._rows):(i+1)*(self._columns * self._rows)])
-        assert(len(self._layers[0])==self._columns * self._rows)
+        assert(len(self._rawData)  % (self._rows * self._columns) == 0)
+        numberOfLayers = int(len(self._rawData) / (self._rows * self._columns))
+        for layer in range(0,numberOfLayers):
+            self._layers.append(self._rawData[layer*(self._rows * self._columns):(layer+1)*(self._rows * self._columns)])
+        assert(len(self._layers[0])==self._rows * self._columns)
     
     def GetLayers(self):
         return self._layers
+        
+    def FlattenLayers(self):
+        #Bore down vertically through the layers at a pixel.  First color encountered is the color.
+        stopValue = 0
+        for rowIndex in range(0,self._rows):
+            for columnIndex in range(0,self._columns):
+                firstColoredPixel = -1
+                for layerIndex in range(0,len(self._layers)):
+                    layer = self._layers[layerIndex]
+                    pixel = int(layer[rowIndex*self._columns + columnIndex])
+                    #The first non-transparent color we find is it.
+                    if (pixel == 1 or pixel == 0):
+                        firstColoredPixel = pixel
+                        break;
+                assert(firstColoredPixel != -1)
+                self._renderLayerPixels.append(firstColoredPixel)
+        return self._renderLayerPixels
+
+    def Render(self):
+        #since we only have black and white, use a character for color.
+        pixels = self.FlattenLayers()
+        renderToText = pixels.copy()
+        #Use '*' as pen, easier to read if we target white (1), so mapping '*' to white
+        renderToText = list(map(lambda x: '*' if x == 1 else ' ',pixels)) 
+        text = []
+        #Render into lines and get list of lines
+        for x in range(0, self._rows):
+            string = ''
+            for y in range(0, self._columns):
+                string = string + renderToText[x * self._columns + y]
+            text.append(string)
+        for line in text:
+            print(line)
+
+
