41d9ec5f229bc9d35bbf93d806d41bd36315fcab
David Wright <720482+dkwgit@users.noreply.github.com>
2019-12-28 15:17:15 -0600

    Part1 of day16

--- Day16/Day16Driver.py
+++ Day16/Day16Driver.py
@@ -3,14 +3,35 @@ from FFT import FFT as FFT
 
 class Day16Driver:
 
-    def __init__(self, basePattern, startMessage):
-        self._startMessage = startMessage
-        self._basePattern = basePattern
 
-    def Run(self):
-        fft = FFT(self._basePattern, self._startMessage)
-        result = fft.DoPhase()
+    def GetFFT(self, basePattern, startMessage):
+        basePattern = list(map(lambda x: int(x),basePattern))
+        startMessage = FFT.ConvertStringToIntArray(startMessage)
+        return FFT(basePattern, startMessage)
+
+    def TestMultiplePhases(self,basePattern,startMessage,resultsPerPhase):
+        fft = self.GetFFT(basePattern, startMessage)
+        for expectedResult in resultsPerPhase:
+            counter,result = fft.DoPhase()
+            assert(expectedResult==FFT.ConvertListToString(result))
+
+    def TestPhaseNumber(self,phase,basePattern,startMessage,expectedLeft8Characters):
+        fft = self.GetFFT(basePattern, startMessage)
+        for x in range(0,phase):
+            (discard, result) = fft.DoPhase()
+        assert(expectedLeft8Characters==FFT.ConvertListToString(result[0:8]))
+
+    def Day16Part1(self,phase,basePattern,startMessage):
+        fft = self.GetFFT(basePattern, startMessage)
+        for x in range(0,phase):
+            (discard, result) = fft.DoPhase()
+        print(f"Day 16 part 1 result: {FFT.ConvertListToString(result[0:8])}")
 
 
-d = Day16Driver(list(map(lambda x : int(x), df.basePattern)), FFT.ConvertStringToIntArray(df.test1[0]))
-d.Run()
+
+d = Day16Driver()
+test1Data = df.test1.copy()
+d.TestMultiplePhases(df.basePattern,test1Data[0],test1Data[1:])
+for phase,message,expectedResult in [df.test2,df.test3,df.test4]:
+    d.TestPhaseNumber(phase,df.basePattern,message,expectedResult)
+d.Day16Part1(100,df.basePattern,df.mainDay16)
--- Day16/FFT.py
+++ Day16/FFT.py
@@ -1,22 +1,42 @@
+import numpy as np
+import itertools
+
 class FFT:
 
     def __init__(self, basePattern, message):
-        self._basePattern = basePattern
-        self._currentMessage = message
+        self._lenMessage = len(message)
+        self._basePattern = np.array(basePattern,np.int64)
+        lenBasePattern = len(self._basePattern)
+        self._currentMessage = np.array(message,np.int64)
+        transformArray = np.zeros((self._lenMessage,self._lenMessage))
+
+        for a in range(0,self._lenMessage):
+            baseMultiplied = itertools.cycle(itertools.chain.from_iterable(itertools.repeat(x, a+1) for x in self._basePattern))
+            for skip in range(0,a+1):
+                next(baseMultiplied)
+            for b in range(a, self._lenMessage):
+                transformArray[a,b] = next(baseMultiplied)
+                
+        self._transformArray = transformArray
         self._phaseCounter = 0
-        self._messageHistory = [ message ]
+        self._messageHistory = [ self._currentMessage ]
         
 
     def DoPhase(self):
 
         newMessage = []
 
-        self._messageHistory.append(self.ConvertToString(newMessage))
-        self._phaseCounter = self.PhaseCounter + 1
+        for a in range(0,self._lenMessage):
+            mult = np.multiply(self._currentMessage,self._transformArray[a])
+            newMessage.append(int(list(str(int(sum(mult))))[-1]))
+
+        self._currentMessage = np.array(newMessage,np.int64)
+        self._messageHistory.append(self._currentMessage)
+        self._phaseCounter = self._phaseCounter + 1
         return (self._phaseCounter,self._messageHistory[-1])
 
     def ConvertListToString(dataArray):
-        return map(lambda x: str(x),dataArray).join('')
+        return "".join(list(map(lambda x: str(x),dataArray)))
 
     def ConvertStringToIntArray(s):
         return list(map(lambda x: int(x), list(s)))
