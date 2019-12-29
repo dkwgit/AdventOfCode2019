720a7afc4c68ec85b8acdbd8f0f75b8842713523
David Wright <720482+dkwgit@users.noreply.github.com>
2019-12-28 18:45:49 -0600

    refactor for greater speed, but still too slow

--- Day16/Day16Driver.py
+++ Day16/Day16Driver.py
@@ -4,10 +4,10 @@ from FFT import FFT as FFT
 class Day16Driver:
 
 
-    def GetFFT(self, basePattern, startMessage):
+    def GetFFT(self, basePattern, startMessage, offset=0):
         basePattern = list(map(lambda x: int(x),basePattern))
         startMessage = FFT.ConvertStringToIntArray(startMessage)
-        return FFT(basePattern, startMessage)
+        return FFT(basePattern, startMessage, offset)
 
     def TestMultiplePhases(self,basePattern,startMessage,resultsPerPhase):
         fft = self.GetFFT(basePattern, startMessage)
@@ -21,17 +21,39 @@ class Day16Driver:
             (discard, result) = fft.DoPhase()
         assert(expectedLeft8Characters==FFT.ConvertListToString(result[0:8]))
 
+    def TestPhaseNumberWithOffset(self,phase,basePattern,startMessage,expectedLeft8Characters):
+        offset = int(startMessage[:7])
+        startMessage = startMessage * 10000
+        fft = self.GetFFT(basePattern, startMessage[offset:], offset)
+        for x in range(0,phase):
+            (discard, result) = fft.DoPhase()
+        assert(expectedLeft8Characters==FFT.ConvertListToString(result[0:8]))
+
     def Day16Part1(self,phase,basePattern,startMessage):
         fft = self.GetFFT(basePattern, startMessage)
         for x in range(0,phase):
             (discard, result) = fft.DoPhase()
         print(f"Day 16 part 1 result: {FFT.ConvertListToString(result[0:8])}")
 
+    def Day16Part2(self,phase,basePattern,startMessage):
+        offset = int(startMessage[:7])
+        startMessage = startMessage * 10000
+        fft = self.GetFFT(basePattern, startMessage[offset:], offset)
+        for x in range(0,phase):
+            (discard, result) = fft.DoPhase()
+        print(f"Day 16 part 2 result: {FFT.ConvertListToString(result[0:8])}")
+
+
+
+
 
 
 d = Day16Driver()
-test1Data = df.test1.copy()
-d.TestMultiplePhases(df.basePattern,test1Data[0],test1Data[1:])
-for phase,message,expectedResult in [df.test2,df.test3,df.test4]:
-    d.TestPhaseNumber(phase,df.basePattern,message,expectedResult)
-d.Day16Part1(100,df.basePattern,df.mainDay16)
+#test1Data = df.test1.copy()
+#d.TestMultiplePhases(df.basePattern,test1Data[0],test1Data[1:])
+#for phase,message,expectedResult in [df.test2,df.test3,df.test4]:
+#    d.TestPhaseNumber(phase,df.basePattern,message,expectedResult)
+#d.Day16Part1(100,df.basePattern,df.mainDay16)  #67481260
+for phase,message,expectedResult in [df.test5,df.test6,df.test7]:
+   d.TestPhaseNumberWithOffset(phase,df.basePattern,message,expectedResult)
+#d.Day16Part2(100,df.basePattern,df.mainDay16)
--- Day16/FFT.py
+++ Day16/FFT.py
@@ -3,32 +3,34 @@ import itertools
 
 class FFT:
 
-    def __init__(self, basePattern, message):
-        self._lenMessage = len(message)
+    def __init__(self, basePattern, message, offset=0):
+        self._offset = offset
         self._basePattern = np.array(basePattern,np.int64)
         lenBasePattern = len(self._basePattern)
         self._currentMessage = np.array(message,np.int64)
-        transformArray = np.zeros((self._lenMessage,self._lenMessage))
-
-        for a in range(0,self._lenMessage):
-            baseMultiplied = itertools.cycle(itertools.chain.from_iterable(itertools.repeat(x, a+1) for x in self._basePattern))
-            for skip in range(0,a+1):
-                next(baseMultiplied)
-            for b in range(a, self._lenMessage):
-                transformArray[a,b] = next(baseMultiplied)
+        self._lenMessage = len(self._currentMessage)
+        print(f"Message length for offset at {offset} is {self._lenMessage}")
                 
-        self._transformArray = transformArray
         self._phaseCounter = 0
         self._messageHistory = [ self._currentMessage ]
         
+    def GetTransformArray(self,index):
+        offset = self._offset
+        transformArray = np.zeros((self._lenMessage),np.int64)
+        baseMultiplied = itertools.chain(itertools.chain.from_iterable(itertools.repeat(x, index+offset+1) for x in self._basePattern[1:]), itertools.cycle(itertools.chain.from_iterable(itertools.repeat(x, index+offset+1) for x in self._basePattern)))
+        for b in range(index, self._lenMessage):
+            transformArray[b] = next(baseMultiplied)
+        return transformArray
 
     def DoPhase(self):
-
+        print(f"On phase {self._phaseCounter}")
         newMessage = []
 
         for a in range(0,self._lenMessage):
-            mult = np.multiply(self._currentMessage,self._transformArray[a])
-            newMessage.append(int(list(str(int(sum(mult))))[-1]))
+            transformArray = self.GetTransformArray(a)
+            mult = np.multiply(self._currentMessage,transformArray)
+            summed = abs(np.sum(mult))
+            newMessage.append(summed % 10)
 
         self._currentMessage = np.array(newMessage,np.int64)
         self._messageHistory.append(self._currentMessage)
