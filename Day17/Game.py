f40e96a63a2614bde6711d676086e073dd3acd7a
David Wright <720482+dkwgit@users.noreply.github.com>
2019-12-31 18:08:44 -0600

    Checking in Day17-2 scaffold walking.  Haven't run my patterns through the computer yet

--- Day17/Game.py
+++ Day17/Game.py
@@ -61,13 +61,13 @@ class Game:
                             [0,1,1,1,0],
                             [0,1,1,1,0],
                             [0,0,0,0,0]]),
-        BT.Right:       np.array([
+        BT.Left:       np.array([
                             [0,0,1,0,0],
                             [0,1,1,1,0],
                             [0,0,1,0,0],
                             [0,0,1,0,0],
                             [0,0,1,0,0]]),
-        BT.Left:        np.array([
+        BT.Right:        np.array([
                             [0,0,1,0,0],
                             [0,0,1,0,0],
                             [0,0,1,0,0],
@@ -143,10 +143,10 @@ class Game:
             if (k==BT.Down): #Fill in sides of arrow, so it looks smooth
                 a[9,23] = a[9,22] = a[9,21] = a[9,20] = a[8,22] = a[8,21] = a[8,20] = a[7,21] = a[7,20] = a[6,20] = color
                 a[15,23] = a[15,22] = a[15,21] = a[15,20] = a[16,22] = a[16,21] = a[16,20] = a[17,21] = a[17,20] = a[18,20] = color
-            if (k==BT.Right): #Fill in sides of arrow, so it looks smooth
+            if (k==BT.Left): #Fill in sides of arrow, so it looks smooth
                 a[4,6] = a[4,7] = a[4,8] = a[4,9] = a[3,7] = a[3,8] = a[3,9] = a[2,8] = a[2,9] = a[1,9] = color
                 a[4,18] = a[4,17] = a[4,16] = a[4,15] = a[3,17] = a[3,16] = a[3,15] = a[2,16] = a[2,15] = a[1,15] = color
-            if (k==BT.Left): #Fill in sides of arrow, so it looks smooth
+            if (k==BT.Right): #Fill in sides of arrow, so it looks smooth
                 a[20,6] = a[20,7] = a[20,8] = a[20,9] = a[21,7] = a[21,8] = a[21,9] = a[22,8] = a[22,9] = a[23,9] = color
                 a[20,18] = a[20,17] = a[20,16] = a[20,15] = a[21,17] = a[21,16] = a[21,15] = a[22,16] = a[22,15] = a[23,15] = color
             Game.table[BT(k)] = a
@@ -219,16 +219,17 @@ class Game:
         
         Fill(self,fillFrom,0)
 
-    def ChangeBlockType(self, pos, newBlockType):
-        self.SetBlockTypeToScreen(pos, newBlockType)
+    def ChangeBlockType(self, pos, newBlockType, batchMode = False):
+        self.SetBlockTypeToScreen(pos, newBlockType, batchMode = False)
 
-    def ChangeColorOfBlock(self, pos, color):
+    def ChangeColorOfBlock(self, pos, color, batchMode = False):
         block = Game.table[self._screen[pos]].copy()
         for y in range(0,Game.blockSize):
             for x in range(0,Game.blockSize):
                 if block[y][x] != 0:
                     block[y][x] = color
-        self.SetBlockToScreen(pos, block)
+        self.SetBlockToScreen(pos, block, batchMode)
+        return pos
 
     #From day 15, leaving it in for now.  Can be use for thinkgs like viewwing blocks,
     #for example by sending a -1
@@ -328,6 +329,30 @@ class Game:
                 self.SetBlockTypeToScreen((y*Game.blockSize,x*Game.blockSize),blockType,True)
         self.BlitAndSendToWindow()
 
+    def GetAdjacentBlocks(self, pos):
+        #Given a block position, what are adjacent N,S,W,E blocks?
+        y,x = pos
+        adjacents = []
+        adjacents.append((y-Game.blockSize, x))
+        adjacents.append((y+Game.blockSize, x))
+        adjacents.append((y, x-Game.blockSize))
+        adjacents.append((y, x+Game.blockSize))
+        return adjacents
+
+    def FindRobotBlock(self):
+        robotBlocks = [ (key,value) for key,value in self._screen.items() if value in [BT.Up, BT.Down, BT.Left, BT.Right, BT.BadRobot]]
+        #should only be in one place, having one of the block value
+        assert(len(robotBlocks)==1)
+        return robotBlocks[0]
+
+    def GetBlocksOfType(self, blockTypeList):
+        return { key:value for key,value in self._screen.items() if value in blockTypeList }
+
+    def GetAdjacentTilesWithFilter(self,pos,filterDict):
+        #Given a tile position, how many other relevant tiles are immediately adjacent.
+        adjacents = self.GetAdjacentBlocks(pos)
+        return [item for item in adjacents if item in filterDict.keys()]
+
     def SearchForJunctions(self):
         #I used colors to visually show progress while searching the scaffold for junctions. Start walking tiles, marking them with a color.
         #
@@ -352,25 +377,15 @@ class Game:
 
         analyzedTiles = {}
 
-        scaffoldTiles = { key:value for key,value in self._screen.items() if value == BT.Wall }
-        #robot is the starting position.
-        robot = [ key for key,value in self._screen.items() if value in [BT.Up, BT.Down, BT.Left, BT.Right]][0]
-
-        def GetAdjacentTiles(pos):
-            #Given a tile position, how many other relevant tiles are immediately adjacent.
-            y,x = pos
-            initial = []
-            initial.append((y-Game.blockSize, x))
-            initial.append((y+Game.blockSize, x))
-            initial.append((y, x-Game.blockSize))
-            initial.append((y, x+Game.blockSize))
-            return [item for item in initial if item in scaffoldTiles.keys()]
+        scaffoldTiles = self.GetBlocksOfType([BT.Wall])
+        robotPos,robotBlockType = self.FindRobotBlock()
 
         def AnalyzeTile(tile, color):
             assert(tile not in analyzedTiles.keys())
 
-            self.ChangeColorOfBlock(tile,color)
-            adjacents = GetAdjacentTiles(tile)
+            if (self._screen[tile]==BT.Wall):
+                self.ChangeColorOfBlock(tile,color)
+            adjacents = self.GetAdjacentTilesWithFilter(tile,scaffoldTiles)
             analyzedTiles[tile] = len(adjacents) #current tile is now analyzed, we record the number of adjacents
 
             nextToAnalyze = [x for x in adjacents if x not in analyzedTiles.keys()]
@@ -411,12 +426,12 @@ class Game:
                             newColor = colors.pop(random.randint(0,len(colors)-1))
                             workers[newColor] = (1,t,newColor)
                             
-                pygame.time.delay(50)  # helps follow the visualization
+                #pygame.time.delay(25)  # helps follow the visualization
 
         #pick a random color to start with.  The random color picking minimizes likelihood of adjacent colors looking alike,
         #making the visualization easier to track with
         color = colors.pop(random.randint(0,len(colors)-1)) 
-        workers[color] = (1,robot,color)
+        workers[color] = (1,robotPos,color)
         DoAnalysis()
         
         junctions = [k for k,v in analyzedTiles.items() if v > 2]
@@ -431,6 +446,133 @@ class Game:
 
         calibration = sum(map(lambda pos: pos[0]*pos[1],junctionsOnTextScreen))
         print(f"Calibration number, the answer for Day17-1: {calibration}")  #6680
+
+    def ResetWallsWhite(self):
+        discard = [ self.ChangeColorOfBlock(k, 0xFFFFFF,True) for k,v in self.GetBlocksOfType([BT.Wall]).items() ]
+        self.BlitAndSendToWindow()
+
+    def WalkScaffold(self):
+        
+        self.SearchForJunctions()
+        self.ResetWallsWhite()
+
+        scaffoldTiles = self.GetBlocksOfType([BT.Wall,BT.Junction])
+        junctionTiles = self.GetBlocksOfType([BT.Junction])
+        walkedHistory = []
+        walked = {}
+
+        def GetNextInDirectionByBlockType(blockType):
+            assert(blockType in [BT.Up,BT.Down,BT.Left,BT.Right])
+            nextStep = None
+            if (blockType == BT.Up):
+                nextStep = (-1,0)
+            if (blockType == BT.Down):
+                nextStep = (1,0)
+            if (blockType == BT.Left):
+                nextStep = (0,-1)
+            if (blockType == BT.Right):
+                nextStep = (0,1)
+            return (nextStep,blockType)
+
+        def TakeStep(robotPos, blockType, moveString):
+            walkedHistory.append(robotPos)
+            if (robotPos not in walked.keys()):
+                walked[robotPos] = 0
+            walked[robotPos] = walked[robotPos] + 1
+            returnValue = None
+            adjacents = self.GetAdjacentTilesWithFilter(robotPos, scaffoldTiles)
+            adjacents = [a for a in adjacents if ((a not in walked.keys()) or (a in junctionTiles.keys()))]
+            if (len(adjacents) == 0):
+                return (robotPos,blockType,moveString)
+            nextStep,nextBlockType = GetNextInDirectionByBlockType(blockType)
+            lengthY,lengthX = nextStep
+            stepBlock = (robotPos[0] + lengthY * Game.blockSize, robotPos[1] + lengthX * Game.blockSize)
+            if (stepBlock not in adjacents):
+                assert(len(adjacents) == 1) #having looked at the scaffold, I don't see intersections at corners
+                a = adjacents[0]
+                stepBlock = a
+                y,x = robotPos
+                sy,sx = stepBlock
+                turnBlockType = None
+                if (sy < y):
+                    turnBlockType = BT.Up
+                    assert(blockType != BT.Down)
+                    moveString = "R" if blockType == BT.Left else "L"
+                if (sy > y):
+                    turnBlockType = BT.Down
+                    assert(blockType != BT.Up)
+                    moveString = "L" if blockType == BT.Left else "R"
+                if (sx < x):
+                    turnBlockType = BT.Left
+                    assert(blockType != BT.Right)
+                    moveString = "L" if blockType == BT.Up else "R"
+                if (sx > x):
+                    turnBlockType = BT.Right
+                    assert(blockType != BT.Left)
+                    moveString = "R" if blockType == BT.Up else "L"
+                assert(turnBlockType is not None)
+                assert(moveString != "")
+                nextBlockType = turnBlockType
+                moveString = moveString + "1"
+                returnValue = (stepBlock,nextBlockType,moveString)
+            else:
+                headString, tailString = moveString[0:-1], moveString[-1]
+                stepCountInDirection = str(int(tailString) + 1)
+                moveString = headString + stepCountInDirection
+                returnValue = (stepBlock,blockType,moveString)
+            return returnValue
+
+        moveString = ""
+        
+        robotPos, nextBlockType = self.FindRobotBlock()
+        assert(nextBlockType in [BT.Up,BT.Down,BT.Left,BT.Right])
+        returnValues = []
+        nextPos = robotPos
+        while(True):
+            if (nextPos not in junctionTiles.keys()):
+                self.ChangeBlockType(nextPos,BT.GreenEmpty)
+            else:
+                self.ChangeBlockType(nextPos,BT.Junction)
+            returnValue = TakeStep(nextPos, nextBlockType, moveString)
+            proposedNextPos,nextBlockType,moveString = returnValue
+            if (proposedNextPos == nextPos):
+                #No more new places to go
+                self.ChangeBlockType(nextPos,nextBlockType)
+                break
+            nextPos = proposedNextPos
+            returnValues.append(returnValue)
+            self.ChangeBlockType(nextPos,nextBlockType)
+            #pygame.time.delay(25)
+
+        def ConsolidateJourneyList(returnValues):
+            outputList = []
+            currentLetter = returnValues[0][2][0]
+            currentItemInDirection = returnValues[0][2]
+            for discard1,discard2,moveString in returnValues:
+                if (moveString[0] != currentLetter):
+                    outputList.append(currentItemInDirection)
+                    currentLetter = moveString[0]
+                currentItemInDirection = moveString
+            outputList.append(currentItemInDirection)
+            return outputList
+
+        journeyList = ConsolidateJourneyList(returnValues)
+        journeyString = ",".join(journeyList)
+
+
+
+        #Writing out the journeystring on paper, 'L8,R10,L8,R8,L12,R8,L8,R10,L8,R8,L8,R10,L8,R10,L8,R10,L8,R8,L12,R8,L8,R10,L12,R8,L12,R8'
+        #yieled the following sub-patterns
+        A = "L8,R10,L8,R8"
+        B = "L12,R8"
+        C = "L8,R10"
+        ALL = "{},{},{},{},{},{},{},{},{},{}".format(A,B,A,C,C,A,B,C,B,B)
+
+        assert(journeyString==ALL)
+
+
+
+        print(f"Scaffold has been walked")
 
     def TranslateToTextScreen(self,pos):
         y,x = pos
@@ -447,21 +589,30 @@ class Game:
                     self.SetDirection(1)
                 elif (event.key == pygame.K_DOWN):
                     self.SetDirection(2)
-                elif (event.key == pygame.K_u):  #self.UpdateDisplay was left over from Day 15.  I rigged 'u' to it to see some new blocks
-                    self.UpdateDisplay(-1)
                 elif (event.key == pygame.K_f): #f does the fill with oxygen on the Day 15 maze
+                    pygame.mouse.set_cursor(*pygame.cursors.broken_x)
                     self.FillWithOxygen()
+                    pygame.mouse.set_cursor(*pygame.cursors.arrow)
                 elif (event.key == pygame.K_j): #j searches for junctions for calibration for Day 17-1
                     #do the visualized search for junctions
+                    pygame.mouse.set_cursor(*pygame.cursors.broken_x)
                     self.SearchForJunctions()
+                    pygame.mouse.set_cursor(*pygame.cursors.arrow)
                 elif (event.key == pygame.K_r): #r reloads the scaffold, by starting the computer over.  You still have to follow up with a space to run it
                     self._started = False
                     computer = Computer()
                     computer.LoadProgram(self._computer.GetOriginalProgram())
                     self._computer = computer
                     self._textScreen = TextScreen() #start over with a new text screen
+                elif (event.key == pygame.K_u):  #self.UpdateDisplay was left over from Day 15.  I rigged 'u' to it to see some new blocks
+                    self.UpdateDisplay(-1)
+                elif (event.key == pygame.K_w):
+                    pygame.mouse.set_cursor(*pygame.cursors.broken_x)
+                    self.WalkScaffold()
+                    pygame.mouse.set_cursor(*pygame.cursors.arrow)
                 elif (event.key == pygame.K_SPACE): #starts the current Computer program (displays the scaffold, etc.)
                     self._started = True if self._started == False else False
+                    pygame.mouse.set_cursor(*pygame.cursors.broken_x)
             
             if event.type == pygame.QUIT: 
                 pygame.quit() 
@@ -487,5 +638,6 @@ class Game:
                     
                     #after we are done accumulating results onto the text screen, draw it to the game screen
                     self.DrawTextScreenToGameScreen()
+                    pygame.mouse.set_cursor(*pygame.cursors.arrow)
             
             
